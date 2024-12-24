017e9e2b8745d5481bb006cbc34a8a4afb04b8c7
Alizah Khalid <khalidaliza237@gmail.com>
2024-12-24 19:52:07 +0500

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       {/* Text Content */}
-      <div className="text-center px-4 py-6 md:py-12 bg-black bg-opacity-50 rounded-lg animate__animated animate__fadeInUp animate__delay-1s">
-        <h1 className="text-2xl sm:text-4xl md:text-6xl font-bold leading-tight animate__animated animate__fadeInRight animate__delay-1s">
+      <div className="text-center lg:mt-5 px-4 py-6 md:py-12 bg-black bg-opacity-50 rounded-lg animate__animated animate__fadeInUp animate__delay-1s">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold leading-tight animate__animated animate__fadeInRight animate__delay-1s">
           Transforming Digital <br /> Ideas into Profitable <br /> Solutions
         </h1>
         <p
@@ -28,7 +28,7 @@ const Hero = () => {
       </div>
 
       {/* Button */}
-      <div className="absolute bottom-5 sm:bottom-10 md:bottom-20 right-5 sm:right-10 md:right-20 animate__animated animate__bounceIn animate__delay-3s">
+      <div className="absolute  sm:bottom-10 md:bottom-20 right-5 sm:right-10 md:right-20 animate__animated animate__bounceIn animate__delay-3s">
         <button className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 bg-[#0d3880] text-white rounded-full flex items-center justify-center shadow-lg hover:bg-[#ccaa0d] transition transform hover:scale-110">
           <span className="text-xs sm:text-sm md:text-base text-center font-bold leading-tight">
             LET'S DISCUSS YOUR PROJECT
